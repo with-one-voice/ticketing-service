c0d11efc45bd37641cee001ad8bbf665e6d65bb9
joon <tjrwns3428@gmail.com>
2025-04-05 07:09:48 +0900

    ci(#2) : ci-settings
    - @ActiveProfiles(test) 적용

--- admin/src/test/java/com/onevoice/admin/AdminApplicationTests.java
+++ admin/src/test/java/com/onevoice/admin/AdminApplicationTests.java
@@ -2,8 +2,10 @@ package com.onevoice.admin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AdminApplicationTests {
 
 	@Test
--- payment/src/test/java/com/onevoice/payment/PaymentApplicationTests.java
+++ payment/src/test/java/com/onevoice/payment/PaymentApplicationTests.java
@@ -2,8 +2,10 @@ package com.onevoice.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PaymentApplicationTests {
 
 	@Test
