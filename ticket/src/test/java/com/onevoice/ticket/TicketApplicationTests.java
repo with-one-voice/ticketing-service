45285a3150c335efab9700ce06562a733bb4dc36
joon <tjrwns3428@gmail.com>
2025-04-05 06:24:34 +0900

    test(#2): ci-settings

--- ticket/src/test/java/com/onevoice/ticket/TicketApplicationTests.java
+++ ticket/src/test/java/com/onevoice/ticket/TicketApplicationTests.java
@@ -1,12 +1,26 @@
 package com.onevoice.ticket;
 
+import jakarta.annotation.PostConstruct;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class TicketApplicationTests {
+
+
+	@Autowired
+	Environment env;
+
+	@PostConstruct
+	void logEnv() {
+		System.out.println("🔎 active profiles: " + Arrays.toString(env.getActiveProfiles()));
+		System.out.println("🔎 spring.config.import: " + env.getProperty("spring.config.import"));
+		System.out.println("🔎 spring.cloud.config.enabled: " + env.getProperty("spring.cloud.config.enabled"));
+	}
 
 	@Test
 	void contextLoads() {
