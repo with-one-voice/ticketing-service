1a44085268d239a701c5bb2e2b5ea77df23a7b87
joon <tjrwns3428@gmail.com>
2025-04-05 01:33:04 +0900

    project init

---
+++ admin/src/test/java/com/onevoice/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onevoice.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
