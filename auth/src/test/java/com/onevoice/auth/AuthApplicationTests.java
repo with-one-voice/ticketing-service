dc29794def4687e4bad85d79b752a9839ca5fbb4
joon <tjrwns3428@gmail.com>
2025-04-05 04:29:01 +0900

    test: ci-settings

--- auth/src/test/java/com/onevoice/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/onevoice/auth/AuthApplicationTests.java
@@ -1,13 +1,27 @@
 package com.onevoice.auth;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+
+import jakarta.annotation.PostConstruct;
+import java.util.Arrays;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthApplicationTests {
+
+	@Autowired
+	Environment env;
+
+	@PostConstruct
+	void init() {
+		System.out.println(">>> Active profile: " + Arrays.toString(env.getActiveProfiles()));
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
 }
