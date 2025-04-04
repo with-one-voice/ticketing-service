d7364c4b3da138757e861680c887ce4c9ec62417
joon <tjrwns3428@gmail.com>
2025-04-05 06:27:32 +0900

    test(#2): ci-settings

--- ticket/src/test/java/com/onevoice/ticket/TicketApplicationTests.java
+++ ticket/src/test/java/com/onevoice/ticket/TicketApplicationTests.java
@@ -1,29 +1,34 @@
 package com.onevoice.ticket;
 
-import jakarta.annotation.PostConstruct;
 import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class TicketApplicationTests {
 
+	private static final Logger log = LoggerFactory.getLogger(TicketApplicationTests.class);
 
 	@Autowired
 	Environment env;
 
-	@PostConstruct
+	@BeforeEach
 	void logEnv() {
-		System.out.println("🔎 active profiles: " + Arrays.toString(env.getActiveProfiles()));
-		System.out.println("🔎 spring.config.import: " + env.getProperty("spring.config.import"));
-		System.out.println("🔎 spring.cloud.config.enabled: " + env.getProperty("spring.cloud.config.enabled"));
+		log.info("🔎 active profiles: {}", Arrays.toString(env.getActiveProfiles()));
+		log.info("🔎 spring.config.import: {}", env.getProperty("spring.config.import"));
+		log.info("🔎 spring.cloud.config.enabled: {}", env.getProperty("spring.cloud.config.enabled"));
 	}
 
 	@Test
 	void contextLoads() {
+		// 빈 테스트
 	}
-
 }
