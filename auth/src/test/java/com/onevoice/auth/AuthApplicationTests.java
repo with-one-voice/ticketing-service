55b0f33643dbc146fcbc7a0009d61e083f72d79e
joon <tjrwns3428@gmail.com>
2025-04-05 07:05:53 +0900

    ci(#2): ci-settings
    - application-test.yml profile설정 삭제

--- auth/src/test/java/com/onevoice/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/onevoice/auth/AuthApplicationTests.java
@@ -1,26 +1,12 @@
 package com.onevoice.auth;
 
-import jakarta.annotation.PostConstruct;
-import java.util.Arrays;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class AuthApplicationTests {
-
-	@Autowired
-	Environment env;
-
-	@PostConstruct
-	void logEnv() {
-		System.out.println("🔎 active profiles: " + Arrays.toString(env.getActiveProfiles()));
-		System.out.println("🔎 spring.config.import: " + env.getProperty("spring.config.import"));
-		System.out.println("🔎 spring.cloud.config.enabled: " + env.getProperty("spring.cloud.config.enabled"));
-	}
 
 	@Test
 	void contextLoads() {
