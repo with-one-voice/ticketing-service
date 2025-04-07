9ca6c29d01f4ced61625b7bca2b777f7e1f720f3
joon <tjrwns3428@gmail.com>
2025-04-07 13:37:35 +0900

    test(#14):add-authentication
    - @ContextConfiguration로 변경

--- auth/src/test/java/com/onevoice/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/onevoice/auth/AuthApplicationTests.java
@@ -3,16 +3,15 @@ package com.onevoice.auth;
 import com.onevoice.auth.application.client.UserClient;
 import com.onevoice.auth.infrastructure.jwt.JwtTokenProvider;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-@Import(AuthApplicationTests.MockConfig.class)
-@EnableFeignClients(clients = {})
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AuthApplicationTests.MockConfig.class)
 class AuthApplicationTests {
 
 	@Test
