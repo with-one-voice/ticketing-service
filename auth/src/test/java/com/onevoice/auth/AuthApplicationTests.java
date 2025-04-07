647bd08706383fec52f06d698d52e954cfe4ae62
joon <tjrwns3428@gmail.com>
2025-04-07 13:32:20 +0900

    test(#14):add-authentication
    - feignclient자동 등록 블록

--- auth/src/test/java/com/onevoice/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/onevoice/auth/AuthApplicationTests.java
@@ -6,19 +6,23 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @Import(AuthApplicationTests.MockConfig.class)
+@EnableFeignClients(clients = {})
 class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 	@TestConfiguration
 	static class MockConfig {
+
 		@Bean
 		public UserClient userClient() {
 			return Mockito.mock(UserClient.class);
