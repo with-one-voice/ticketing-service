506b91ec3b2c4b0be53bc04f8d0e90bff3639046
joon <tjrwns3428@gmail.com>
2025-04-07 13:00:19 +0900

    test(#14):add-authentication
    - auth test : userClient 목Mock 객체 생성

--- auth/src/test/java/com/onevoice/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/onevoice/auth/AuthApplicationTests.java
@@ -1,12 +1,33 @@
 package com.onevoice.auth;
 
+import com.onevoice.auth.application.client.UserClient;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(AuthApplicationTests.MockConfig.class)
 class AuthApplicationTests {
+
+	private final UserClient userClient;
+
+	AuthApplicationTests(UserClient userClient) {
+		this.userClient = userClient;
+	}
+
 
 	@Test
 	void contextLoads() {
+	}
+
+	@TestConfiguration
+	static class MockConfig {
+		@Bean
+		public UserClient userClient() {
+			return Mockito.mock(UserClient.class);
+		}
 	}
 }
