258ec62dc7f130d2fa533042eaac37527094e055
Yeonnnny <kdoy030@gmail.com>
2025-04-08 23:14:05 +0900

    test(#29) : 주석 처리

--- user/src/test/java/com/onevoice/user/UserApplicationTests.java
+++ user/src/test/java/com/onevoice/user/UserApplicationTests.java
@@ -1,16 +1,13 @@
 package com.onevoice.user;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 class UserApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-
-
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
 }
