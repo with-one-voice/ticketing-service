265e17a0a127092eef6a898cb2cb737288647f61
joon <tjrwns3428@gmail.com>
2025-04-08 16:03:23 +0900

    fix(#30): 단위 테스트로ã 변경
    - admin, user 도메인 테스트 코드 변경

--- user/src/main/java/com/onevoice/user/presentation/UserController.java
+++ user/src/main/java/com/onevoice/user/presentation/UserController.java
@@ -1,11 +1,16 @@
 package com.onevoice.user.presentation;
 
+import com.onevoice.common.dto.CommonResponse;
 import com.onevoice.user.application.dto.FindUserQuery;
 import com.onevoice.user.application.dto.LoginRequestDto;
 import com.onevoice.user.application.dto.SignupRequestDto;
 import com.onevoice.user.application.service.UserService;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +31,17 @@ public class UserController {
     @PostMapping("/login")
     public Optional<FindUserQuery> login(@RequestBody LoginRequestDto command) {
         return userService.login(command);
+    }
+
+    @Secured("ROLE_USER")
+    @GetMapping("/test")
+    public ResponseEntity<CommonResponse<String>> testMethod(){
+        return CommonResponse.success("호날두 사랑해");
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/ronaldo")
+    public ResponseEntity<CommonResponse<String>> testRonaldo(){
+        return CommonResponse.success("유기체 역사상 GOAT");
     }
 }
