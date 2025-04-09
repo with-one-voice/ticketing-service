ee0ac1aca30e6b2cb03d3e2682c4df50e160dc61
lime1st <lime1st@gmail.com>
2025-04-09 17:30:01 +0900

    feat(#40): add for validate - Controller 에 Valid 추가

--- notification/src/main/java/com/onevoice/notification/presentation/NotificationController.java
+++ notification/src/main/java/com/onevoice/notification/presentation/NotificationController.java
@@ -4,22 +4,18 @@ import com.onevoice.common.dto.CommonResponse;
 import com.onevoice.common.dto.ResponseCode;
 import com.onevoice.notification.application.service.NotificationService;
 import com.onevoice.notification.presentation.dto.request.CreateNotificationRequest;
+import jakarta.validation.Valid;
 import java.net.URI;
-import java.security.Principal;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -35,7 +31,7 @@ public class NotificationController {
      */
     @PostMapping
     public ResponseEntity<?> post(
-        @RequestBody CreateNotificationRequest request,
+        @RequestBody @Valid CreateNotificationRequest request,
         @AuthenticationPrincipal UUID userId
     ) {
         log.info("Request Body:{}", request);
