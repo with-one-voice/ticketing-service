f84960523c82cedafb536cf8a1d7cf4fd4a07815
lime1st <lime1st@gmail.com>
2025-04-12 03:27:48 +0900

    feat(#64): add mail server check - redis 캐싱을 추가하여 mail server check 를 지정된 시간에만 하도록 수정

---
+++ notification/src/main/java/com/onevoice/notification/infrastructure/config/CacheConfig.java
@@ -0,0 +1,43 @@
+package com.onevoice.notification.infrastructure.config;
+
+import java.time.Duration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+        RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(5)) // 캐시 TTL 설정 (5분)
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                new StringRedisSerializer()))
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(factory)
+            .cacheDefaults(configuration)
+            .build();
+    }
+}
--- notification/src/main/java/com/onevoice/notification/infrastructure/message/EmailServerChecker.java
+++ notification/src/main/java/com/onevoice/notification/infrastructure/message/EmailServerChecker.java
@@ -2,10 +2,11 @@ package com.onevoice.notification.infrastructure.message;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.Session;
-import jakarta.mail.Store;
+import jakarta.mail.Transport;
 import java.util.Properties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Slf4j(topic = "EmailServerChecker")
@@ -18,18 +19,32 @@ public class EmailServerChecker {
     @Value("${spring.mail.port}")
     private String MAIL_PORT;
 
-    // 메일 서버 상태 체크
+    @Value("${spring.mail.username}")
+    private String MAIL_USERNAME;
+
+    @Value("${spring.mail.password}")
+    private String MAIL_PASSWORD;
+
+    // 메일 서버 상태 체크(Redis Cache TTL: 5 minute)
+    @Cacheable(value = "mailServerAvailability")
     public boolean isMailServerAvailable() {
         Properties props = new Properties();
         props.put("mail.smtp.host", MAIL_HOST); // 메일 서버 호스트
         props.put("mail.smtp.port", MAIL_PORT); // 메일 서버 포트
-        props.put("mail.smtp.connectiontimeout", "3000"); // 타임아웃 설정 (3초)
+        props.put("mail.smtp.auth", "true"); // SMTP 인증 사용
+        props.put("mail.smtp.connectiontimeout", "3000"); // 접속 타임아웃 설정 (3초)
         props.put("mail.smtp.timeout", "3000"); // 읽기 타임아웃 설정 (3초)
+        props.put("mail.smtp.starttls.enable", "true"); // TLS 사용
 
         Session session = Session.getDefaultInstance(props);
 
-        try (Store store = session.getStore("smtp")) {
-            store.connect();
+        try (Transport transport = session.getTransport("smtp")) {
+            transport.connect(
+                MAIL_HOST,
+                Integer.parseInt(MAIL_PORT),
+                MAIL_USERNAME,
+                MAIL_PASSWORD);
+            log.info("Mail server is available");
             return true;
         } catch (MessagingException e) {
             log.error("Mail server is not available: {}", e.getMessage());
