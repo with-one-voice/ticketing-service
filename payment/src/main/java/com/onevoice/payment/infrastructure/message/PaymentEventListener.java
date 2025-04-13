d225546fcfbcbbffdba29e068dd16f6d30361dc5
lime1st <lime1st@gmail.com>
2025-04-14 01:39:04 +0900

    feat(#64): add kafka producer - kafka topic 발행 기능 추가

--- payment/src/main/java/com/onevoice/payment/infrastructure/message/PaymentEventListener.java
+++ payment/src/main/java/com/onevoice/payment/infrastructure/message/PaymentEventListener.java
@@ -1,11 +1,15 @@
 package com.onevoice.payment.infrastructure.message;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onevoice.payment.application.client.TicketClient;
-import com.onevoice.payment.application.dto.client.UpdateTicketStatusRequestDto;
-import com.onevoice.payment.application.dto.client.UpdateTicketStatusRequestDto.TicketStatus;
+import com.onevoice.payment.application.dto.message.TicketMessage;
+import com.onevoice.payment.application.dto.message.TicketMessage.TicketStatus;
 import com.onevoice.payment.application.event.PaymentCreateEvent;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -16,25 +20,49 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class PaymentEventListener {
 
     private final TicketClient ticketClient;
+    private final ObjectMapper objectMapper;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     // 데이터 불일치를 방지하기 위해 트랜잭션이 커밋된 이후에만 실행하도록 설정
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleEventAfterCommit(PaymentCreateEvent event) {
+    public void handleEventAfterCommit(PaymentCreateEvent event) throws JsonProcessingException {
         log.info("PaymentCreateEvent received after commit.");
 
         // 트랜잭션 커밋 후 실행되는 로직
-        ticketClient.updateTicketStatus(
-            event.getTicketId(),
-            new UpdateTicketStatusRequestDto(TicketStatus.CONFIRM_PAYMENT));
+        // kafka topic 발행으로 변경
+//        ticketClient.updateTicketStatus(
+//            event.getMessage().ticketId(),
+//            new UpdateTicketStatusRequestDto(TicketStatus.CONFIRM_PAYMENT));
+        String message = objectMapper.writeValueAsString(event.getMessage());
+        String ticketStatus = getTicketStatus(
+            event.getMessage().ticketId(),
+            TicketStatus.CONFIRM_PAYMENT
+        );
+        // key 는 지정하지 않아도 된다.
+        kafkaTemplate.send("notification", message);
+        kafkaTemplate.send("ticket_status", ticketStatus);
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
-    public void handleEventAfterRollback(PaymentCreateEvent event) {
+    public void handleEventAfterRollback(PaymentCreateEvent event) throws JsonProcessingException {
         log.info("PaymentCreateEvent received after rollback.");
 
         // 트랜잭션 롤백 후 실행되는 로직
-        ticketClient.updateTicketStatus(
-            event.getTicketId(),
-            new UpdateTicketStatusRequestDto(TicketStatus.CANCELLED));
+//        ticketClient.updateTicketStatus(
+//            event.getMessage().ticketId(),
+//            new UpdateTicketStatusRequestDto(TicketStatus.CANCELLED));
+        String ticketStatus = getTicketStatus(
+            event.getMessage().ticketId(),
+            TicketStatus.CANCELLED
+        );
+        kafkaTemplate.send("ticket_status", ticketStatus);
+    }
+
+    private String getTicketStatus(UUID ticketId, TicketStatus cancelled)
+        throws JsonProcessingException {
+        return objectMapper.writeValueAsString(
+            new TicketMessage(ticketId,
+                cancelled)
+        );
     }
 }
