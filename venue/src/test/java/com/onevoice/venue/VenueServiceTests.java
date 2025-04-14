f663145d576ad6d4ba52aaba4e2984b528a91204
Yeonnnny <kdoy030@gmail.com>
2025-04-14 23:21:50 +0900

    refact(#69) : @ExtendWith 어노테이션 추가

--- venue/src/test/java/com/onevoice/venue/VenueServiceTests.java
+++ venue/src/test/java/com/onevoice/venue/VenueServiceTests.java
@@ -13,16 +13,17 @@ import java.util.Optional;
 import java.util.UUID;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.ArgumentMatchers.any;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class VenueServiceTests {
 
     @InjectMocks
@@ -30,11 +31,6 @@ public class VenueServiceTests {
 
     @Mock
     private VenueRepository venueRepository;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Test
     @DisplayName("공연장 등록 성공")
