188e0a46a5c5d392a1b14fdad97625f93cf0b0e9
lime1st <lime1st@gmail.com>
2025-04-15 11:11:05 +0900

    style(#74): change field name etc...

--- notification/src/main/java/com/onevoice/notification/infrastructure/message/EmailSender.java
+++ notification/src/main/java/com/onevoice/notification/infrastructure/message/EmailSender.java
@@ -9,10 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 @Slf4j(topic = "EmailSender")
-@Service
+@Component
 @RequiredArgsConstructor
 public class EmailSender {
 
