9e37182222f7ff8ae7d92b03a1716c36aaddc2b2
chaewKim <anjh7127@gmail.com>
2025-04-17 21:32:29 +0900

    feat(#95) : add kafka topic - 선점 만료 후 스케쥴링 로직에 만료 토빅 발행 추가

--- seat/src/main/java/com/onevoice/seat/infrastructure/scheduler/SeatHoldRecoveryScheduler.java
+++ seat/src/main/java/com/onevoice/seat/infrastructure/scheduler/SeatHoldRecoveryScheduler.java
@@ -1,5 +1,6 @@
 package com.onevoice.seat.infrastructure.scheduler;
 
+import com.onevoice.seat.infrastructure.message.SeatEventProducer;
 import com.onevoice.seat.infrastructure.redis.RedisKeyUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,15 +8,14 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class SeatHoldRecoveryScheduler {
     private final StringRedisTemplate redisTemplate;
+    private final SeatEventProducer seatEventProducer;
 
     @Scheduled(fixedDelay = 300_000) // 5분마다
     public void recoverExpiredHolds() {
@@ -39,6 +39,12 @@ public class SeatHoldRecoveryScheduler {
             //좌석별 상태 확인
             Map<Object, Object> seatStatusMap = redisTemplate.opsForHash().entries(redisKey);
 
+
+            // userId → 만료된 seatId 리스트
+            //각 사용자마다 만료된 좌석들 모아두는 자료구조
+            Map<UUID, List<UUID>> expiredSeatsByUser = new HashMap<>();
+
+
             for (Map.Entry<Object, Object> entry : seatStatusMap.entrySet()) {
                 String seatIdStr = (String) entry.getKey();
                 String status = (String) entry.getValue();
@@ -51,8 +57,25 @@ public class SeatHoldRecoveryScheduler {
                     if (Boolean.FALSE.equals(stillExists)) {
                         redisTemplate.opsForHash().put(redisKey, seatIdStr, "AVAILABLE");
                         log.info("TTL 만료 → [{}] 상태 복구 완료 (sessionId: {})", seatId, sessionId);
+
+                        //userId 확인
+                        String userIdStr = redisTemplate.opsForValue().get(holdKey);
+                        if(userIdStr != null) {
+                            UUID userId = UUID.fromString(userIdStr);
+                            if (!expiredSeatsByUser.containsKey(userId)) {
+                                expiredSeatsByUser.put(userId, new ArrayList<>());
+                            }
+                            expiredSeatsByUser.get(userId).add(seatId);
+                        }
                     }
                 }
+            }
+            // Kafka 발행
+            for (Map.Entry<UUID, List<UUID>> entry : expiredSeatsByUser.entrySet()) {
+                UUID userId = entry.getKey();
+                List<UUID> expiredSeats = entry.getValue();
+
+                seatEventProducer.sendSeatExpired(expiredSeats, userId);
             }
         }
     }
