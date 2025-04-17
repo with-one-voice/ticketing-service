def0f9ec0c30d24cd058fa13a7da410ef5914040
chaewKim <anjh7127@gmail.com>
2025-04-17 15:32:59 +0900

    feat(#94) : kafka 토픽 수정

--- common/src/main/java/com/onevoice/common/enumtype/KafkaTopicType.java
+++ common/src/main/java/com/onevoice/common/enumtype/KafkaTopicType.java
@@ -9,9 +9,10 @@ public enum KafkaTopicType {
     TICKET_CONFIRM("ticket_confirm"),
     SEAT_CONFIRM("seat_confirm"),
     PAYMENT_FAIL("payment_fail"),
-    TICKET_CONFIRM_FAIL("ticket_confirm_fail"),
-    SEAT_CONFIRM_FAIL("seat_confirm_fail"),
+    TICKET_CANCEL("ticket_cancel"),
+    SEAT_CANCEL("seat_cancel"),
     SEAT_CREATE_REQUEST("seat_create_request"),
+    SEAT_EXPIRED("seat_expired"),
     SEAT_CREATE_SUCCESS("seat_create_success"),
     SEAT_CREATE_FAIL("seat_create_fail");
 
