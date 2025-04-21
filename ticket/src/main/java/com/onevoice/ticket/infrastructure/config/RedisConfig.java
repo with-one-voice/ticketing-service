98da471fa57943e766bd3e206ae3ee4a224dd8a0
joon <tjrwns3428@gmail.com>
2025-04-22 07:07:04 +0900

    feat(#91): common모듈 redis설정 삭제

--- ticket/src/main/java/com/onevoice/ticket/infrastructure/config/RedisConfig.java
+++ ticket/src/main/java/com/onevoice/ticket/infrastructure/config/RedisConfig.java
@@ -1,13 +1,14 @@
 package com.onevoice.ticket.infrastructure.config;
 
-import com.onevoice.common.config.manualConfig.RedisSupportFactory;
-
-import com.onevoice.common.config.manualConfig.RedissonSupportFactory;
+import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 public class RedisConfig {
 
@@ -19,17 +20,26 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return RedisSupportFactory.redisConnectionFactory(REDIS_HOST, REDIS_PORT);
+        return new LettuceConnectionFactory(REDIS_HOST, REDIS_PORT);
     }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-        return RedisSupportFactory.redisTemplate(factory);
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
     }
 
     @Bean
     public RedissonClient redissonClient() {
-        return RedissonSupportFactory.create(REDIS_HOST,REDIS_PORT);
+        Config config = new Config();
+        config.useSingleServer()
+            .setAddress("redis://" + REDIS_HOST + ":" + REDIS_PORT)
+            .setConnectionMinimumIdleSize(5)
+            .setConnectionPoolSize(10);
+        return Redisson.create(config);
     }
 
 }
