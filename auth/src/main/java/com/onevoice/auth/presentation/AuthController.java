1d11e7fba70d9125ff73fe8121839221b5f6cb67
lime1st <lime1st@gmail.com>
2025-04-23 14:51:49 +0900

    feat(#110): add auth service api - oauth2 로그인 성공/실패 api 추가

--- auth/src/main/java/com/onevoice/auth/presentation/AuthController.java
+++ auth/src/main/java/com/onevoice/auth/presentation/AuthController.java
@@ -7,13 +7,17 @@ import com.onevoice.auth.presentation.dto.response.LoginResponseDto;
 import com.onevoice.auth.presentation.dto.response.SignupResponseDto;
 import com.onevoice.common.dto.CommonResponse;
 import com.onevoice.common.dto.ResponseCode;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
@@ -34,8 +38,26 @@ public class AuthController {
 
         String token = authService.login(requestDto);
         return ResponseEntity.ok()
-            .header(HttpHeaders.AUTHORIZATION, "Bearer " + token).body(CommonResponse.createCommonResponse(
-                ResponseCode.SUCCESS,new LoginResponseDto("로그인 성공")));
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+            .body(CommonResponse.createCommonResponse(
+                ResponseCode.SUCCESS, new LoginResponseDto("로그인 성공")));
     }
 
+    @GetMapping("/oauth2/success")
+    public ResponseEntity<CommonResponse<LoginResponseDto>> redirectToGoogleLoginSuccess(
+        HttpServletResponse response) {
+        log.info("Successfully oauth2 login");
+        return ResponseEntity.ok()
+            .header(HttpHeaders.AUTHORIZATION, response.getHeader("Authorization"))
+            .body(CommonResponse.createCommonResponse(
+                ResponseCode.SUCCESS, new LoginResponseDto("로그인 성공")));
+    }
+
+    @GetMapping("/oauth2/failure")
+    public ResponseEntity<CommonResponse<String>> failure() {
+        log.info("OAuth2 failure");
+        return ResponseEntity.status(403)
+            .body(CommonResponse.createCommonResponse(
+                ResponseCode.FORBIDDEN, "로그인 실패"));
+    }
 }
