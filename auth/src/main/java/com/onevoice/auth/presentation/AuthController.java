5d593b4809c1e965003ab499f04b9d2af1f84ff1
lime1st <lime1st@gmail.com>
2025-04-25 13:05:33 +0900

    feat(#119): add logout controller - /logout 기능 추가 - 레디스에 블랙리스트로 등록

--- auth/src/main/java/com/onevoice/auth/presentation/AuthController.java
+++ auth/src/main/java/com/onevoice/auth/presentation/AuthController.java
@@ -1,21 +1,21 @@
 package com.onevoice.auth.presentation;
 
 import com.onevoice.auth.application.service.AuthService;
+import com.onevoice.auth.application.service.RedisService;
 import com.onevoice.auth.presentation.dto.request.LoginRequestDto;
 import com.onevoice.auth.presentation.dto.request.SignupRequestDto;
 import com.onevoice.auth.presentation.dto.response.LoginResponseDto;
 import com.onevoice.auth.presentation.dto.response.SignupResponseDto;
 import com.onevoice.common.dto.CommonResponse;
 import com.onevoice.common.dto.ResponseCode;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHeaders;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
-    private final RedisTemplate<String, String> redisTemplate;
+    private final RedisService redisService;
 
     @PostMapping("/signup")
     public ResponseEntity<CommonResponse<SignupResponseDto>> signup(
@@ -46,13 +46,25 @@ public class AuthController {
                 ResponseCode.SUCCESS, new LoginResponseDto("로그인 성공")));
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<CommonResponse<String>> logout(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String token,
+        @RequestHeader("X-Token-Expires") String expires
+    ) {
+        redisService.blacklistToken(token, Long.parseLong(expires));
+        return ResponseEntity.ok()
+            .body(CommonResponse.createCommonResponse(
+                ResponseCode.SUCCESS, "로그 아웃"
+            ));
+    }
+
     @GetMapping("/oauth2/success")
     public ResponseEntity<CommonResponse<LoginResponseDto>> success(
         @RequestParam("key") String key) {
-        String token = Optional.ofNullable(redisTemplate.opsForValue().get(key))
+        String token = redisService.get(key)
             .map(jwt -> {
                 // 토큰을 읽어온 후 삭제
-                redisTemplate.delete(key);
+                redisService.remove(key);
                 return jwt;
             }).orElse(null);
         if (token == null) {
