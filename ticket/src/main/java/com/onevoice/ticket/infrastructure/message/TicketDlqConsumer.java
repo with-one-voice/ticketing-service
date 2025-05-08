99e912b723f5025b13f1e216d1730985c727572b
joon <tjrwns3428@gmail.com>
2025-05-08 10:03:01 +0900

    feat(#175): add ticket DLQ
    - ticket dlqì설정 추가
    - kafka메시지 이ë이벤트 오류 발생 시 , DLQ를 통해 로그 수집

--- ticket/src/main/java/com/onevoice/ticket/infrastructure/message/TicketDlqConsumer.java
+++ ticket/src/main/java/com/onevoice/ticket/infrastructure/message/TicketDlqConsumer.java
@@ -1,5 +1,14 @@
 package com.onevoice.ticket.infrastructure.message;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onevoice.ticket.application.dto.message.PaymentCreateMessage;
+import com.onevoice.ticket.application.dto.message.PaymentFailMessage;
+import com.onevoice.ticket.application.dto.message.PaymentSuccessMessage;
+import com.onevoice.ticket.application.dto.message.SeatExpiredMessage;
+import com.onevoice.ticket.application.service.TicketService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -7,13 +16,69 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class TicketDlqConsumer {
 
-    @KafkaListener(topics = "payment_success.DLQ", groupId = "ticket-dlq-group")
-    public void consumeDlq(ConsumerRecord<String, String> record) {
+    private final ObjectMapper objectMapper;
+
+    /**
+     * 현재는 log만 기록했는데, Slack이나 이메일 등을 통해 알림을 설정하는 것도 좋아 보입니다.
+     *
+     */
+
+    @KafkaListener(topics = "payment_create.DLQ", groupId = "ticket-dlq-group")
+    public void consumePaymentCreateDLQ(ConsumerRecord<String,String> record) throws JsonProcessingException {
         log.error("[DLQ] Failed message received: topic={}, partition={}, offset={}, key={}, value={}",
             record.topic(), record.partition(), record.offset(), record.key(), record.value());
 
-        // 필요 시 file 또는 DB 저장도??
+        JsonNode root = objectMapper.readTree(record.value());
+        JsonNode payload = root.get("payload");
+        PaymentCreateMessage message = objectMapper.treeToValue(payload,
+            PaymentCreateMessage.class);
+
+        log.warn("[payment_success] Received :{}", message);
     }
+
+    @KafkaListener(topics = "payment_success.DLQ", groupId = "ticket-dlq-group")
+    public void consumePaymentSuccessDLQ(ConsumerRecord<String, String> record) throws JsonProcessingException {
+        log.error("[DLQ] Failed message received: topic={}, partition={}, offset={}, key={}, value={}",
+            record.topic(), record.partition(), record.offset(), record.key(), record.value());
+
+        JsonNode root = objectMapper.readTree(record.value());
+        JsonNode payload = root.get("payload");
+
+        PaymentSuccessMessage message = objectMapper.treeToValue(payload,
+            PaymentSuccessMessage.class);
+
+        log.warn("[payment_success] Received :{}", message);
+    }
+
+    @KafkaListener(topics = "payment_fail.DLQ", groupId = "ticket-dlq-group")
+    public void consumePaymentFailDLQ(ConsumerRecord<String, String> record) throws JsonProcessingException {
+        log.error("[DLQ] Failed message received: topic={}, partition={}, offset={}, key={}, value={}",
+            record.topic(), record.partition(), record.offset(), record.key(), record.value());
+
+        JsonNode root = objectMapper.readTree(record.value());
+        JsonNode payload = root.get("payload");
+
+        PaymentFailMessage message = objectMapper.treeToValue(payload,
+            PaymentFailMessage.class);
+
+        log.warn("[payment_success] Received :{}", message);
+    }
+
+    @KafkaListener(topics = "payment_fail.DLQ", groupId = "ticket-dlq-group")
+    public void consumeSeatExpiredDLQ(ConsumerRecord<String, String> record) throws JsonProcessingException {
+        log.error("[DLQ] Failed message received: topic={}, partition={}, offset={}, key={}, value={}",
+            record.topic(), record.partition(), record.offset(), record.key(), record.value());
+
+        JsonNode root = objectMapper.readTree(record.value());
+        JsonNode payload = root.get("payload");
+
+        SeatExpiredMessage message = objectMapper.treeToValue(payload,
+            SeatExpiredMessage.class);
+
+        log.warn("[payment_success] Received :{}", message);
+    }
+
 }
